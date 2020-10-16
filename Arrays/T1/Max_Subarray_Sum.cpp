e8fa01ae6f0f6e35d3209ab86f34e1f738d0169f
Anupam <anusrivastava910@gmail.com>
2020-10-16 23:49:20 +0530

    Arrays :fire:

--- Arrays/T1/Max_Subarray_Sum.cpp
+++ Arrays/T1/Max_Subarray_Sum.cpp
@@ -1,6 +1,7 @@
 //  Time Complexity : O(N)
-//  Auxilary Space : O()
+//  Auxilary Space : O(1)
 //  Problem Statement : Return Maximum Subarray Sum
+// Kadane's Algorithm
 #include <iostream>
 #include <climits>
 #include <algorithm>
@@ -8,13 +9,21 @@ using namespace std;
 
 int maxSubarraySum(int *arr, int n)
 {
+    int res = arr[0], maxEnding = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        maxEnding = max(maxEnding + arr[i], arr[i]);
+        res = max(res, maxEnding);
+    }
+    return res;
 }
 
 int main()
 {
-    int arr[] = {1, 0, 1, 1, 1, 1, 0, 1, 1};
+    int arr[] = {-6, -1, -8};
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << maxSubarraySum(arr, n) << '\n';
 
     return 0;
 }
+//IPL20QL5TR8JCT
